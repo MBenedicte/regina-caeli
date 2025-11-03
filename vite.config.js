8700afb9f21de1c2e455e6bf3d7c7e5b8c602852
Benedicte Musabimana <benedicte.gokind@gmail.com>
2025-11-03 16:41:37 +0100

    updated

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [vue()],
-  base: "/",
+  base: "/regina-caeli/",
   resolve: {
     alias: { "@": path.resolve(__dirname, "src") },
   },
